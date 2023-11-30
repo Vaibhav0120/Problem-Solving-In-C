1ee164dd6ce6a173e946baeb766f6ec61872c5f1
Vaibhav0120 <150184948+Vaibhav0120@users.noreply.github.com>
2023-11-30 21:48:29 +0530

    lab 10

---
+++ Lab 10/Search_v2.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,f,n,elt,loc;
+    printf("Enter N:");
+    scanf("%i",&n);
+
+    int a[100];
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter %ith element:",i);
+        scanf("%i",&a[i]);
+    }
+
+    printf("Enter ELT:");
+    scanf("%i",&elt);
+
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==elt)
+        {
+            f=1;
+            loc=i;
+            break;
+        }
+    }
+
+    if(f==1)
+        printf("Found %i at location %i",elt,loc);
+    else
+        printf("%i not found in array",elt);
+
+    return 0;
+}
---
+++ Lab 10/Selective_sort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,n,t;
+    printf("Enter N:");
+    scanf("%i",&n);
+
+    int a[100];
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter %ith element:",i);
+        scanf("%i",&a[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        printf(" [%i] ",a[i]);
+    }
+
+    return 0;
+}
