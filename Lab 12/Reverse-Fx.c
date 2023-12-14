b567b1694f754399fe519c463d57321f79aa3d26
Vaibhav0120 <150184948+Vaibhav0120@users.noreply.github.com>
2023-12-14 16:40:04 +0530

    push

--- Lab 12/Reverse-Fx.c
+++ Lab 12/Reverse-Fx.c
@@ -1,1 +1,31 @@
-//CODE MILL NAHI RAHA ;_;
+//NOTE : THIS WAS OPTIONAL
+//THIS IS JUST AN IDEA
+//IT'S NOT WORKING CORRECTLY
+#include<stdio.h>
+#include<math.h>
+
+int reverse(int n,int i);
+
+void main()
+{
+    int n,i;
+
+    printf("Enter the value of N:");
+    scanf("%i", &n);
+
+    printf("Enter number of digits:");
+    scanf("%i", &i);
+
+    int result = reverse(n,i);
+
+    printf("Reverse of number: %d\n", result);
+}
+
+int reverse(int n,int i)
+{
+    int lastDigit = n % 10;
+
+    int position = (int)pow(10, i - 1);
+
+    return (lastDigit * position + reverse(n / 10, i - 1));
+}
