4a49f8846d5d074a48b5255a13e7daa68d9a5166
Vaibhav <150184948+Vaibhav0120@users.noreply.github.com>
2024-01-16 14:46:26 +0000

    fixes

--- Lab 06 || Special Series/prime_no.c
+++ Lab 06 || Special Series/prime_no.c
@@ -8,18 +8,18 @@ int main ()
     scanf("%i",&n);
 
     i=2,c=0;
-        while (i<n/2)
+        while (i < n/2)
         {
-            if(n%i==0)
+            if(n % i == 0)
             {
-                c=1;
+                c = 1;
                 break;
             }
             else
             i++;
         }
 
-    if(c==1)
+    if(c == 1)
         printf("%i is NOT a Prime No",n);
     else
         printf("%i is a Prime No",n);
