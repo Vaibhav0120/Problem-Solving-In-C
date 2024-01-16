924bffee3ea8c13b2541d367f9cfd20aee24bccd
Vaibhav <150184948+Vaibhav0120@users.noreply.github.com>
2024-01-16 14:23:17 +0000

    changes

--- Lab 08 || Special Series Range/CHALLENGE_2.0.c
+++ Lab 08 || Special Series Range/CHALLENGE_2.0.c
@@ -3,21 +3,23 @@
 int fact(int n);
 void main()
 {
-    int i,f=1,sum = 0;
+    int i,f = 1,sum = 0;
 
-    for(i=1;i<=7;i++)
-        sum = sum + (i/fact(i));
+    for(i = 1;i <= 7; i++)
+        sum = sum + ( i / fact(i) );
 
     printf("Sum: %i\n", sum);
 }
 
 int fact(int n)
 {
-    int i,f=1;
-    for(i=1;i<=7;i++)
+    int i , f = 1;
+    
+    for(i = 1;i <= n; i++)
     {
-        f=f*i;
+        f = f * i;
     }
+
     return f;
 }
 
