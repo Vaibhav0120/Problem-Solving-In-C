46850b64d6c35dbdf4b0d814893b32b7cc3c2923
Vaibhav <150184948+Vaibhav0120@users.noreply.github.com>
2024-01-16 14:48:56 +0000

    changes

--- Lab 12 || Functions/Reverse-Fx.c
+++ Lab 12 || Functions/Reverse-Fx.c
@@ -1,24 +1,30 @@
-//NOTE: TRY TO RUN IT ON ONLINE C COMPILER IF NOT WORKING CORRECTLY ON VS CODE
 #include<stdio.h>
 
 int reverse(int n, int reversedNum);
 
 int main()
 {
-int n,reversedNum;
+int n,reversedNum=0;
+
 printf("Enter the value of N:");
 scanf("%i", &n);
+
 reversedNum = reverse(n, reversedNum);
+
 printf("Reverse of number: %i", reversedNum);
+
 return 0;
 }
 
-int reverse(int n, int reversedNum){
-if (n == 0)
-return reversedNum;
-else{
-int lastDigit;
-lastDigit = n % 10;
-reversedNum = reversedNum * 10 + lastDigit;
-return reverse(n / 10, reversedNum);}
+int reverse(int n, int reversedNum)
+{
+    if (n == 0)
+    return reversedNum;
+    else
+    {
+        int lastDigit;
+        lastDigit = n % 10;
+        reversedNum = reversedNum * 10 + lastDigit;
+        return reverse(n / 10, reversedNum);
+    }
 }
