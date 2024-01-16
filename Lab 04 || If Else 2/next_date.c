fae183df74dcbc11a97b98e3072bb91ba817ef20
Vaibhav <150184948+Vaibhav0120@users.noreply.github.com>
2024-01-16 15:27:13 +0000

    next date

--- Lab 04 || If Else 2/next_date.c
+++ Lab 04 || If Else 2/next_date.c
@@ -10,50 +10,50 @@ int main()
     printf("Enter Year: ");
     scanf("%i",&y);
 
-    if (m==1||m==3||m==5||m==7||m==8||m==10)
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10)
     {
-        if(d<31)
+        if(d < 31)
         {
-           d=d+1;
+           d = d + 1;
         }
-        else if (d==31)
+        else if (d == 31)
         {
-            d=1; 
-            m=m+1;
+            d = 1; 
+            m = m + 1;
         }
         else
         {
             printf("date is not valid");
         }
     }
-    else if(m==4||m==6||m==9||m==11)
+    else if(m == 4 || m == 6 || m == 9 || m == 11)
     {
-        if(d<30)
+        if(d < 30)
         {
-            d=d+1;
+            d = d + 1;
         }
-        else if (d=30)
+        else if (d == 30)
         {
-            d=1;
-            m=m+1;
+            d = 1;
+            m = m + 1;
         }
         else
         {
             printf("date is not valid");
         }
     }
-    else if(m==2)
+    else if(m == 2)
     {
-        if(y%4==0 && y%100!=0 || y%400==0)
+        if(y % 4==0 && y % 100 != 0 || y % 400 == 0)
         {
-           if(d<29)
+           if(d < 29)
         {
-            d=d+1;
+            d = d + 1;
         }
         else if (d==29)
         {
-            d=1;
-            m=m+1;
+            d = 1;
+            m = m + 1;
         }
         else
         {
@@ -62,14 +62,14 @@ int main()
         }
         else
         {
-            if(d<28)
+            if(d < 28)
         {
             d=d+1;
         }
-        else if (d==28)
+        else if (d == 28)
         {
-            d=1;
-            m=m+1;
+            d = 1;
+            m = m + 1;
         }
         else
         {
@@ -77,17 +77,17 @@ int main()
         }
         }
     }
-    else if(m==12)
+    else if(m == 12)
     {
-        if(d<31)
+        if(d < 31)
         {
-            d=d+1;
+            d = d+1;
         }
-        else if (d==31)
+        else if (d == 31)
         {
-            d=1;
-            m=1;
-            y=y+1;
+            d = 1;
+            m = 1;
+            y = y+1;
         }
         else
         {
@@ -96,7 +96,7 @@ int main()
     }
 else
 {
-    if (d<1 || 0 < m < 12 || y < 1)
+    if (d < 1 || 0 < m || m > 12 || y < 1)
     {
         printf("date is not valid");
     }
