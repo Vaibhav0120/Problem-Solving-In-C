f6091eb4822f592482a2163a45e685039831b133
Vaibhav <150184948+Vaibhav0120@users.noreply.github.com>
2024-03-19 16:17:41 +0530

    Update prime_no.c

--- 07 || Special Series/prime_no.c
+++ 07 || Special Series/prime_no.c
@@ -7,19 +7,18 @@ int main ()
     printf("Enter N: ");
     scanf("%i",&n);
 
-    i=2,c=0;
         while (i < n/2)
         {
             if(n % i == 0)
             {
-                c = 1;
+                c = 0;
                 break;
             }
             else
             i++;
         }
 
-    if(c == 1)
+    if(c == 0)
         printf("%i is NOT a Prime No",n);
     else
         printf("%i is a Prime No",n);
