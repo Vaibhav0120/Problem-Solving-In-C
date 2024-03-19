3ec6c9bb305778d561d9a4931f2188dbbf1a9ff7
Vaibhav <150184948+Vaibhav0120@users.noreply.github.com>
2024-03-19 16:21:15 +0530

    Update Prime_500.c

--- 08 || Special Series Range/Prime_500.c
+++ 08 || Special Series Range/Prime_500.c
@@ -2,22 +2,22 @@
 
 int main()
 {
-    int i=2,c=0;
+    int i=2,c=1;
 
     for(int n = 1;n < 500;n++)
     {
-        i=2,c=0;
+        i=2,c=1;
         while (i<n/2)
         {
             if(n%i==0)
             {
-                c=1;
+                c=0;
                 break;
             }
             else
             i++;
         }
-        if(c==1)
+        if(c==0)
             printf("");
         else
             printf("Prime no:%d\n",n);    
