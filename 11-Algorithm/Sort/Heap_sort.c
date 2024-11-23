1e21977c82b72d606989a9baa433d525f53a7b16
Vaibhav Sharma <vaibhavsh0120@gmail.com>
2024-11-23 23:46:45 +0530

    added sorts

---
+++ 11-Algorithm/Sort/Heap_sort.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+// To heapify a subtree rooted with node i
+// which is an index in arr[].
+void heapify(int arr[], int n, int i) {
+
+    // Initialize largest as root
+    int largest = i; 
+
+    // left index = 2*i + 1
+    int l = 2 * i + 1; 
+
+    // right index = 2*i + 2
+    int r = 2 * i + 2;
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest]) {
+        largest = l;
+    }
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest]) {
+        largest = r;
+    }
+
+    // If largest is not root
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// Main function to do heap sort
+void heapSort(int arr[], int n) {
+
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i > 0; i--) {
+
+        // Move current root to end
+        int temp = arr[0]; 
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        // Call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+// A utility function to print array of size n
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Driver's code
+int main() {
+    int arr[] = {9, 4, 3, 8, 10, 2, 5}; 
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    heapSort(arr, n);
+
+    printf("Sorted array is \n");
+    printArray(arr, n);
+    return 0;
+}
--- 11-Algorithm/Sort/Quick_sort.c
+++ 11-Algorithm/Sort/Quick_sort.c
@@ -2,11 +2,19 @@
 
 void swap(int* a, int* b);
 
+// Partition function
 int partition(int arr[], int low, int high) {
     
+    // Choose the pivot
     int pivot = arr[high];
+    
+    // Index of smaller element and indicates 
+    // the right position of pivot found so far
     int i = low - 1;
 
+    // Traverse arr[low..high] and move all smaller
+    // elements to the left side. Elements from low to 
+    // i are smaller after every iteration
     for (int j = low; j <= high - 1; j++) {
         if (arr[j] < pivot) {
             i++;
@@ -14,15 +22,21 @@ int partition(int arr[], int low, int high) {
         }
     }
     
+    // Move pivot after smaller elements and
+    // return its position
     swap(&arr[i + 1], &arr[high]);  
     return i + 1;
 }
 
+// The QuickSort function implementation
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
         
+        // pi is the partition return index of pivot
         int pi = partition(arr, low, high);
 
+        // Recursion calls for smaller elements
+        // and greater or equals elements
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
